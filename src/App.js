59d921d5c220d9e0d5842dd47430305bb46e793e
Derald Abbe <deraldabbe1@gmail.com>
2023-07-17 13:33:46 -0600

    add fragments,portals, and refs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from "react";
 
-import AddUser from './Components/Users/AddUser';
-import UsersList from './Components/Users/UsersList';
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 function App() {
   const [usersList, setUsersList] = useState([]);
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString() }];
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
-  }
+  };
+
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
